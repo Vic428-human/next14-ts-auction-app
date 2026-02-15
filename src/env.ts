a3c39056cda2f5f8332fb00763e376fb859cc09b
Vic428-human <z0983195379a@gmail.com>
2026-02-16 04:44:40 +0800

    set env var

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
-    OPEN_AI_API_KEY: z.string().min(1),
+    NODE_ENV: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
@@ -13,7 +13,7 @@ export const env = createEnv({
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+    NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
