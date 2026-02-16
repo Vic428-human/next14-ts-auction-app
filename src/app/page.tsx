d5451ca69dce3a80959be285ec1dfdc73d8706bd
Vic428-human <z0983195379a@gmail.com>
2026-02-16 08:33:27 +0800

    顯示所有bits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,34 @@ import Image from "next/image";
 // we're going to import database we have (from  db/database.ts)
 import { database } from "@/db/database";
 
-import { bids } from "@/db/schema";
+import { bids as bidsSchema } from "@/db/schema";
 
 
-export default function Home() {
+export default async function Home() {
+
+  // fetch all the bids from the database
+  const bids = await database.query.bids.findMany();
+
+
   return (
     <main>
       <form action={async (formData: FormData) => {
         'use server';
         // this is the drizzle library tool we can use
         // import bids that schema we have ex: src/db/schema.ts
-        await database.insert(bids).values({});
+        await database.insert(bidsSchema).values({});
       }}>
         {/* connect to the database using drizzle */}
         <input name="bid" placeholder="Bid" />
         <button type="submit">place bit</button>
       </form>
+
+      {/* show all the bids */}
+      {bids.map((bid) => (
+        <div key={bid.id}>
+          <p>{bid.id}</p>
+        </div>
+      ))}
     </main>
   );
 }
